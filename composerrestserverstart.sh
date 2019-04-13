82badf6a0134a441f5c3942fe61e1c246dd6434a
unknown <bandaru.nagesh@gmail.com>
2019-04-13 19:39:45 +0530

    HTML Changes

--- composerrestserverstart.sh
+++ composerrestserverstart.sh
@@ -6,5 +6,5 @@ cd ./emr
 composer network install --card PeerAdmin@hlfv1 --archiveFile emr@0.0.1.bna
 composer network start --networkName emr --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer network ping --card admin@emr
-composer-rest-server -c admin@emr -n never -w true > composer-rest-server.out 2> composer-rest-server.err < /dev/null &
+nohup composer-rest-server -c admin@emr -n never -w true > composer-rest-server.out 2> composer-rest-server.err < /dev/null &
 
