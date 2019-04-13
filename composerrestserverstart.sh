4d7fb979d846ee115063ee882e96e42f7a4ea926
unknown <bandaru.nagesh@gmail.com>
2019-04-13 17:31:51 +0530

    update revoke changes

--- composerrestserverstart.sh
+++ composerrestserverstart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#
-#
-
+#Custom Script developed to run the rest server automatically after restart of the system
+$PWD/fabric-dev-servers/stopFabric.sh
 $PWD/fabric-dev-servers/startFabric.sh
-cd $PWD/emr
+cd ./emr
 composer network install --card PeerAdmin@hlfv1 --archiveFile emr@0.0.1.bna
 composer network start --networkName emr --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer network ping --card admin@emr
 composer-rest-server -c admin@emr -n never -w true > composer-rest-server.out 2> composer-rest-server.err < /dev/null &
+
